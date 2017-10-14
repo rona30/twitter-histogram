0954615416605217cdd4a49ab75def8e905f8793
rona30 <32729506+rona30@users.noreply.github.com>
2017-10-14 17:44:22 +0800

    Create tweet_data.php

---
+++ tweet_data.php
@@ -0,0 +1,23 @@
+<?php
+require_once('TwitterAPIExchange.php');
+// Set access tokens
+$settings = array(
+'oauth_access_token' => "918975920962072577-VjEe76BkTnVW6uPsedbSDP0DGrmV1So",
+'oauth_access_token_secret' => "5yvAbnctdtMYUy7pz20FutUoZGGiPBJpZXMoQLpaBYQR6",
+'consumer_key' => "7NL3foKJ5FMzQDGq2Nqe54aFE",
+'consumer_secret' => "1cllXAkGF6y4z8cP8KStDQybFQmbXwPMyzf9x8ufHVmp0CDWhE"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "imronajss";}
+if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 500;}
+$getfield = "?screen_name=$user&count=$count";
+$twitter = new TwitterAPIExchange($settings);
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+echo "<pre>";
+print_r($string);
+echo "</pre>";
+?>
