b52ccc922acc28b68a6ee5a8262036c48013c4f3
rona30 <32729506+rona30@users.noreply.github.com>
2017-10-15 00:17:50 +0800

    Update tweet_data.php

--- tweet_data.php
+++ tweet_data.php
@@ -25,20 +25,18 @@ $string = json_decode($twitter->setGetfield($getfield)
 
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
    
-//get and format the time (AM or PM)
-$tweettime = array();       
+//get and format the time (AM or PM)      
 foreach($string as $key => $row)
 {
   $thetime[$key]  = date("g", strtotime($row['created_at']));
-  $thenotation[$key] = date("A", strtotime($items['created_at']));   
+  $thenotation[$key] = date("A", strtotime($row['created_at']));   
 }
 
 // Sort the data with notation ascending
-// Add $data as the last parameter, to sort by the common key
+// Add $string as the last parameter, to sort by the common key
 array_multisort($thenotation, SORT_ASC, $thetime, SORT_ASC, $string);
 
 sort($string);
-
-$tweettime = implode(',',$tweettime);
+$tweettime = implode(',',$string);
 echo($tweettime);
 ?>
