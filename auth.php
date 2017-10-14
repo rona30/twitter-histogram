d7ff78ee7aae77a94be621b51eca6f852aa1e47f
rona30 <32729506+rona30@users.noreply.github.com>
2017-10-14 10:42:04 +0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,14 +2,16 @@
 require_once('TwitterAPIExchange.php');
 // Set access tokens
 $settings = array(
-'oauth_access_token' => "	918975920962072577-VjEe76BkTnVW6uPsedbSDP0DGrmV1So",
+'oauth_access_token' => "918975920962072577-VjEe76BkTnVW6uPsedbSDP0DGrmV1So",
 'oauth_access_token_secret' => "5yvAbnctdtMYUy7pz20FutUoZGGiPBJpZXMoQLpaBYQR6",
 'consumer_key' => "YOUR_CONSUMER_KEY",
 'consumer_secret' => "YOUR_CONSUMER_SECRET"
 );
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 $requestMethod = "GET";
-$getfield = '?screen_name=imronajss&count=50';
+if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "imronajss";}
+if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 500;}
+$getfield = "?screen_name=$user&count=$count";
 $twitter = new TwitterAPIExchange($settings);
 $string = json_decode($twitter->setGetfield($getfield)
 ->buildOauth($url, $requestMethod)
